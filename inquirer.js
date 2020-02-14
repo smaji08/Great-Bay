f2ed84325d01305b0da5b11599f6e8bb1c32e521
Rekha Ballur <rnmadalli@gmail.com>
2020-02-13 21:19:12 -0500

    inquirer

---
+++ inquirer.js
@@ -0,0 +1,27 @@
+const inquirer = require("inquirer");
+inquirer.prompt([{
+    type: "list",
+    message: "what do you want to do today",
+    name: "stack",
+    choices: [
+        "Post an Item!!",
+        "Bid on an item!!"
+    ]
+}]).then(async function (data) {
+    if (data.stack === "Post an Item!!") {
+        console.log("Hi you are posting an Item");
+        inquirer.prompt([{
+                type: "input",
+                name: "name",
+                message: "Please enter the name of an Item",
+            },
+            {
+                type: "input",
+                name: "price",
+                message: "Please enter the price of an Item"
+            }
+
+        ])
+    }
+    else(console.log("You chose to Bid on an item"));
+});
